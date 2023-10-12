3bed7b6b14f1ddf3d350944f1a43c384557dd65c
ChinossMP <jmonroyp004@alumno.uaemex.mx>
2023-10-12 10:26:58 -0600

    actualizacion

--- src/Triangulo.java
+++ src/Triangulo.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
+public class Triangulo implements FiguraGeometrica {
+    private double base;
+    private double altura;
 
-public class Triangulo extends interfaz {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.println("Calculadora de Área de Triángulo");
-        
-        System.out.print("Ingresa la longitud de la base del triángulo: ");
-        double base = input.nextDouble();
-        
-        System.out.print("Ingresa la altura del triángulo: ");
-        double altura = input.nextDouble();
-        
-        // Calcula el área del triángulo
-        double area = (base * altura) / 2;
-        
-        System.out.println("El área del triángulo es: " + area);
+    // Constructor
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    // Implementación del método calcularArea de la interfaz FiguraGeometrica
+    @Override
+    public double calcularArea() {
+        return 0.5 * base * altura;
     }
 }
-
